7115b9b7f0e52c2525e364a13f28d6a163783009
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-12-20 01:02:18 -0300

    feat: add new table

--- app/schema.sql
+++ app/schema.sql
@@ -1,9 +1,15 @@
-CREATE TABLE timers (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                user_id TEXT NOT NULL,
-                channel_id TEXT NOT NULL,
-                ship_name TEXT NOT NULL,
-                timer_end TIMESTAMP NOT NULL,
-                notified BOOLEAN DEFAULT FALSE
-            );
-CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE IF NOT EXISTS timers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT NOT NULL,
+    channel_id TEXT NOT NULL,
+    ship_name TEXT NOT NULL,
+    timer_end TIMESTAMP NOT NULL,
+    notified BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS star_notifications (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT NOT NULL,
+    username TEXT NOT NULL
+);
+
