ff90807d06712138c2136f1345933bd18eddeb78
Renan Cavalcante <renancavalcantercb@protonmail.com>
2025-02-04 03:21:25 -0300

    feat: add logs

--- app/commands/curse_word.py
+++ app/commands/curse_word.py
@@ -1,6 +1,11 @@
 from discord.ui import Button, View
 import discord
-from db import add_or_update_curse_counter, get_curse_counters, undo_last_curse_counter as db_undo_last_curse_counter
+from db import (
+    add_or_update_curse_counter,
+    get_curse_counters,
+    undo_last_curse_counter as db_undo_last_curse_counter,
+)
+from utils import log_event
 
 curse_word_emojis = {
     "😡": "Pedro",
@@ -25,6 +30,14 @@ async def create_buttons(ctx):
             await add_or_update_curse_counter(emoji, name)
             counters = await get_curse_counters()
             updated_count = next((row[2] for row in counters if row[0] == emoji), 0)
+
+            log_event(
+                "info",
+                f"Button {name} ({emoji}) clicked.",
+                user_name=interaction.user.name,
+                user_id=interaction.user.id,
+            )
+
             await interaction.response.send_message(
                 f"{name} now has {updated_count} curse words!", ephemeral=False
             )
@@ -36,6 +49,14 @@ async def create_buttons(ctx):
 
     async def undo_callback(interaction):
         last_undone = await db_undo_last_curse_counter()
+
+        log_event(
+            "info",
+            "Button Undo Last clicked.",
+            user_name=interaction.user.name,
+            user_id=interaction.user.id,
+        )
+
         if last_undone:
             emoji, name = last_undone
             await interaction.response.send_message(
@@ -49,6 +70,13 @@ async def create_buttons(ctx):
     undo_button.callback = undo_callback
     view.add_item(undo_button)
 
+    log_event(
+        "info",
+        "Command `create_buttons` called.",
+        user_name=ctx.message.author.name,
+        user_id=ctx.message,
+    )
+
     await ctx.send(
         "👀 Curse Word Tracker! Click a button to add a curse word:", view=view
     )
@@ -59,6 +87,13 @@ async def show_rankings(ctx):
     Displays the rankings of curse word counters from the database.
     """
     counters = await get_curse_counters()
+
+    log_event(
+        "info",
+        "Command `show_rankings` called.",
+        user_name=ctx.message.author.name,
+        user_id=ctx.message.author.id,
+    )
 
     if not counters:
         await ctx.send("No curse word data found!")
